cc50d5f2bf6d5f7ea048e2739b2beca8366f939c
MD-RAUFUR-RAHIM <90611116+MD-RAUFUR-RAHIM@users.noreply.github.com>
2023-02-13 17:15:06 +0600

    Add files via upload

---
+++ Assignment2/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int id=101;
+        String title="Iphone15";
+        int price=1895;
+        String description="euroes ";
+        String catagorey="Phone";
+        System.out.println("Id: "+101);
+        System.out.println("Title: "+title);
+        System.out.println("Description: "+description);
+        System.out.println("Catagorey: "+catagorey);
+    }
+}
